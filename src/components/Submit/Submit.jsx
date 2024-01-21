4afd140aa89502e2876dd7fe9bc3e1b27a91c9fa
Gavin Popken <gavinpopkenart@gmail.com>
2024-01-21 15:30:48 -0600

    finished the loop

--- src/components/Submit/Submit.jsx
+++ src/components/Submit/Submit.jsx
@@ -1,18 +1,22 @@
 import axios from "axios"
 import { Typography, Button, Grid } from "@mui/material"
 import { useSelector } from "react-redux"
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min"
 
 const Submit = () => {
     const formInputs = useSelector(store => store)
+    const history = useHistory()
     const { feeling, understanding, support, comments } = formInputs
 
     const handleSubmit = () => {
         console.log('posting to axios');
+        history.push('/success')
     }
 
     return (
         <>
             <Typography variant="h3" align="center">Review Your Feedback!</Typography>
+            <Typography variant="h4" align="center">Thank you for answering!</Typography>
             <Grid container justifyContent={'center'}>
                 <Grid item>
                     <Typography variant="body1">Feelings: {feeling}</Typography>
