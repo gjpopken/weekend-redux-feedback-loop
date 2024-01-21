cf183592a968bfa0f8ddf09f82fe207738283b4d
Gavin Popken <gavinpopkenart@gmail.com>
2024-01-20 23:14:05 -0600

    got the routes to work

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import axios from 'axios';
-import Feelings from '../Page/Page';
+import Page from '../Page/Page';
 import { Container } from '@mui/material';
+import { HashRouter as HR, Route } from 'react-router-dom/cjs/react-router-dom.min';
 import './App.css';
 
 function App() {
@@ -15,7 +16,41 @@ function App() {
         </header>
       </div>
       <Container>
-        <Feelings />
+        <HR>
+          <Route path='/' exact>
+            <Page
+              question={'How are you feeling today?'}
+              inputType={'number'}
+              inputLabel={'Feeling?'}
+              path={'/understanding'}
+            />
+          </Route>
+          <Route path='/understanding'>
+            <Page
+              question={'How well are you understanding the content?'}
+              inputType={'number'}
+              inputLabel={'Understanding?'}
+              path={'/support'}
+            />
+          </Route>
+          <Route path='/support'>
+            <Page
+              question={'How well are you being supported?'}
+              inputType={'number'}
+              inputLabel={'Support?'}
+              path={'/comments'}
+            />
+          </Route>
+          <Route path='/comments'>
+            <Page
+              question={'Any comments you want to leave?'}
+              inputType={'text'}
+              inputLabel={'Comments?'}
+              path={'/submit'}
+            />
+          </Route>
+        </HR>
+
       </Container>
     </>
 
--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -1,19 +1,21 @@
 import { Typography, TextField, Grid, Button } from "@mui/material"
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min"
 
-const Feelings = () => {
+const Page = ({question, inputType, inputLabel, btnText, path}) => {
+const history = useHistory()
 
     return (
         <>
-            <Typography variant="h3" align="center">How are you feeling today?</Typography>
+            <Typography variant="h3" align="center">{question}</Typography>
             <Grid container justifyContent='center' margin={'dense'}>
-                <TextField type="number" variant="outlined" size="medium" label="Feeling?" margin="dense" inputProps={{ 'data-testid': 'input' }} />
+                <TextField type={inputType} variant="outlined" size="medium" label={inputLabel} margin="dense" inputProps={{ 'data-testid': 'input' }} />
             </Grid>
             <Grid container justifyContent={'center'}>
-                <Button variant="contained" size="large" data-testid="next" style={{marginTop: '5px'}}>Next</Button>
+                <Button variant="contained" size="large" data-testid="next" style={{marginTop: '5px'}} onClick={() => history.push(path)}>{btnText || 'Next'}</Button>
             </Grid>
         </>
 
     )
 }
 
-export default Feelings
+export default Page
